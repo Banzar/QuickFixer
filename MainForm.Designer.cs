f7fe97d84296720357a73a24a1637157abc48ba3
David Ledford <dcledford@ymail.com>
2013-01-21 11:56:07 -0500

    first commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,148 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: David Ledford
+ Date: 11/11/2012
+ * Time: 9:50 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace QuickFixer
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(3, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(282, 20);
+			this.textBox1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(210, 38);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Get File";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(100, 76);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(185, 20);
+			this.textBox2.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(3, 79);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(91, 17);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Text to Replace:";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(13, 102);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(77, 17);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Replace With:";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(100, 102);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(185, 20);
+			this.textBox3.TabIndex = 5;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(200, 128);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(85, 23);
+			this.button2.TabIndex = 6;
+			this.button2.Text = "Save Changes";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(3, 130);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(282, 16);
+			this.richTextBox1.TabIndex = 7;
+			this.richTextBox1.Text = "";
+			this.richTextBox1.Visible = false;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(290, 161);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Name = "MainForm";
+			this.Text = "QuickFixer";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: David Ledford
+ Date: 11/11/2012
+ * Time: 9:50 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace QuickFixer
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			openFileDialog1.ShowDialog();
+			string myFileChoice = openFileDialog1.FileName;
+			textBox1.Text = myFileChoice;	
+		}
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			if (textBox1.Text != "")
+			{
+				System.IO.StreamReader myFile =
+					new System.IO.StreamReader(textBox1.Text);
+				string myFileContents = myFile.ReadToEnd();
+				myFile.Close();
+			
+				richTextBox1.Text = myFileContents.Replace(textBox2.Text, textBox3.Text);
+				
+				System.IO.StreamWriter myFileEdited =
+					new System.IO.StreamWriter(textBox1.Text);
+				myFileEdited.Write(richTextBox1.Text);
+				myFileEdited.Close();
+			}
+
+		}
+	}
+}
